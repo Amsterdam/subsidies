535900d9e2dd6454d55a149d619baa60c5cb7138
Niels Prins <niels.prins@datavibes.nl>
2016-10-31 10:50:58 +0100

    nieuwe datafile
    Signed-off-by: Niels Prins <niels.prins@datavibes.nl>

--- app/static/custom_js/feiten.js
+++ app/static/custom_js/feiten.js
@@ -35,7 +35,6 @@ function parseISO8601(dateStringInRange) {
 var isoExp = /^\s*(\d{4})-(\d\d)-(\d\d)\s*$/,
     date = new Date(NaN), month,
     parts = isoExp.exec(dateStringInRange);
-
     if(parts) {
       month = +parts[2];
       date.setFullYear(parts[1], month - 1, parts[3]);
