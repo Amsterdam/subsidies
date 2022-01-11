85eecec98c88ba266584ca93d1773d048ec0939d
Jasper Swart <jasper.g.swart@gmail.com>
2022-01-11 15:47:33 +0100

    added list part 1

--- src/Lijst/Index.tsx
+++ src/Lijst/Index.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Table, TableCell, TableBody, TableHeader, TableRow } from "@amsterdam/asc-ui";
+
 import { useSubsidieContext } from "../DataProvider";
 import PageTemplate from "../PageTemplate";
 
@@ -12,6 +14,50 @@ const Lijst = () => {
   const { data, isLoading } = useSubsidieContext();
   // TODO: Define the correct type for the filters object
   const [filters, setFilters] = useState({});
+  console.log("liijst", data);
+
+  // <Table>
+  //   <TableHeader>
+  //     <TableRow>
+  //       <TableCell as="th">One</TableCell>
+  //       <TableCell as="th">Two</TableCell>
+  //       <TableCell as="th">Three</TableCell>
+  //       <TableCell as="th">Four</TableCell>
+  //     </TableRow>
+  //   </TableHeader>
+  //   <TableBody>
+  //     <TableRow>
+  //       <TableCell>A looooooong text</TableCell>
+  //       <TableCell>1</TableCell>
+  //       <TableCell>
+  //         <Link variant="inline" href="/">
+  //           Lorem ipsum dolor sit amet.
+  //         </Link>
+  //       </TableCell>
+  //       <TableCell>2000</TableCell>
+  //     </TableRow>
+  //     <TableRow>
+  //       <TableCell>Foo</TableCell>
+  //       <TableCell>1</TableCell>
+  //       <TableCell>Baz</TableCell>
+  //       <TableCell>2000</TableCell>
+  //     </TableRow>
+  //   </TableBody>
+  // </Table>
+
+  // AANVRAGER: "(Particulier)"
+  // BEDRAG_AANGEVRAAGD: 4000
+  // BEDRAG_VASTGESTELD: 4000
+  // BEDRAG_VERLEEND: 4000
+  // BELEIDSTERREIN: "Welzijn en Zorg"
+  // DATUM_OVERZICHT: "2022-01-10 01:00:59.190"
+  // DOSSIERNUMMER: "SBA-000019"
+  // ORGANISATIEONDERDEEL: "stadsdeel Zuid"
+  // PROJECT_NAAM: "Jazzclub New Orleans"
+  // REGELINGNAAM: "Subsidieregeling Bewonersinitiatieven"
+  // SUBSIDIEJAAR: "2016"
+  // TABLE_NAME: "SB_MI_SUBSIDIEREGISTER"
+  // TYPE_PERIODICITEIT: "Eenmalig"
 
   const filteredData = filter(filters, data);
 
@@ -19,16 +65,43 @@ const Lijst = () => {
     <PageTemplate>
       <div>Lijst test</div>
       {!isLoading && (
-        <div>
-          {filteredData.map((d) => (
-            <p key={d.DOSSIERNUMMER}>
-              <span>{d.DOSSIERNUMMER}</span>
-              <span>{d.PROJECT_NAAM}</span>
-              <span>{d.SUBSIDIEJAAR}</span>
-              <span>{d.BEDRAG_VERLEEND}</span>
-            </p>
-          ))}
-        </div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableCell as="th">Project en naam</TableCell>
+              <TableCell as="th">Regeling en organisatie</TableCell>
+              <TableCell as="th">Thema</TableCell>
+              <TableCell as="th">Jaar</TableCell>
+              <TableCell as="th">Soort</TableCell>
+              <TableCell as="th">Aangevraagd</TableCell>
+              <TableCell as="th">Verleend</TableCell>
+              <TableCell as="th">Vastgesteld</TableCell>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {filteredData.map((d) => (
+              <TableRow key={d.DOSSIERNUMMER}>
+                <TableCell>
+                  {d.PROJECT_NAAM}
+                  <br />
+                  {d.AANVRAGER}
+                </TableCell>
+                <TableCell>
+                  {d.REGELINGNAAM}
+                  <br />
+                  {d.ORGANISATIEONDERDEEL}
+                </TableCell>
+                <TableCell>{d.BELEIDSTERREIN}</TableCell>
+                <TableCell>{d.SUBSIDIEJAAR}</TableCell>
+                <TableCell>{d.TYPE_PERIODICITEIT}</TableCell>
+                <TableCell>{d.BEDRAG_AANGEVRAAGD}</TableCell>
+                <TableCell>{d.BEDRAG_VERLEEND}</TableCell>
+                <TableCell>{d.BEDRAG_VASTGESTELD}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       )}
     </PageTemplate>
   );
