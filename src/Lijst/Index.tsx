fe24edc70251869b0b373afd93068ca8862df579
janthijs <92784122+janthijs@users.noreply.github.com>
2022-01-11 09:52:22 +0100

    fix: add filter function

--- src/Lijst/Index.tsx
+++ src/Lijst/Index.tsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import { useSubsidieContext } from "../DataProvider";
 import PageTemplate from "../PageTemplate";
 
+// TODO: Move this function to a file
+function filter(filters, data) {
+  return data;
+}
+
 const Lijst = () => {
+  // TODO: Maybe move this hook to the filter function? Maybe make that function a hook?
   const { data, isLoading } = useSubsidieContext();
+  // TODO: Define the correct type for the filters object
+  const [filters, setFilters] = useState({});
+
+  const filteredData = filter(filters, data);
 
   return (
     <PageTemplate>
       <div>Lijst test</div>
       {!isLoading && (
         <div>
-          {data.map((d) => (
+          {filteredData.map((d) => (
             <p key={d.DOSSIERNUMMER}>
               <span>{d.DOSSIERNUMMER}</span>
               <span>{d.PROJECT_NAAM}</span>
