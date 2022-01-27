2771ae1a871e83df6036650fefed19062f871c85
Jasper Swart <jasper.g.swart@gmail.com>
2022-01-27 13:29:00 +0100

    removed text mock

--- src/Hooks/useGetLatestUpdateDate.test.ts
+++ src/Hooks/useGetLatestUpdateDate.test.ts
@@ -1,25 +1,21 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from "@testing-library/react-hooks";
 import axios from "axios";
 
 import useGetLatestUpdateDate from "./useGetLatestUpdateDate";
 
-const mock = `TABLE_NAME;DOSSIERNUMMER;AANVRAGER;REGELINGNAAM;BELEIDSTERREIN;ORGANISATIEONDERDEEL;PROJECT_NAAM;TYPE_PERIODICITEIT;BEDRAG_AANGEVRAAGD;BEDRAG_VERLEEND;BEDRAG_VASTGESTELD;SUBSIDIEJAAR;DATUM_OVERZICHT
-----------;-------------;---------;------------;--------------;--------------------;------------;------------------;------------------;---------------;------------------;------------;---------------
-SB_MI_SUBSIDIEREGISTER;SBA-000019;(Particulier);Subsidieregeling Bewonersinitiatieven;Welzijn en Zorg;stadsdeel Zuid;Jazzclub New Orleans;Eenmalig;4000.00;4000.00;4000.00;2016;2022-01-24 01:01:04.587
-SB_MI_SUBSIDIEREGISTER;SBA-000042;St. Centrale Dorpenraad Landelijk Noord;Begrotingspost Subsidies SD Noord (Organisatiespecifiek);Algemeen;stadsdeel Noord;Stichting Centrale Dorpenraad Landelijk Noord;Periodiek;83280.00;83280.00;83280.00;2016;2022-01-24 01:01:04.587
-`;
+const mock = {};
 
 jest.mock("axios");
 
 describe("useGetLatestUpdateDate", () => {
   it("renders correctly", () => {
     // @ts-ignore
-    axios.get.mockResolvedValueOnce(mock );
+    axios.get.mockResolvedValueOnce({ data: mock });
 
-    const { result } = renderHook(() => useGetLatestUpdateDate())
+    const { result } = renderHook(() => useGetLatestUpdateDate());
 
-    console.log('result', result.current);
-    
+    console.log("result", result.current);
+
     expect(1).toBe(1);
   });
 });
