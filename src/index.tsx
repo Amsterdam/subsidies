de5b98e72bfbeebcfd0fe48175e86d7c7220c0cb
Jan Thijs <92784122+janthijs@users.noreply.github.com>
2022-09-21 16:23:14 +0200

    fix: new font and package bumps (#184)

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { GlobalStyle, ThemeProvider } from "@amsterdam/asc-ui";
+import { createRoot } from "react-dom/client";
+import { ascDefaultTheme, GlobalStyle, ThemeProvider } from "@amsterdam/asc-ui";
 
 import App from "./App";
 import { SubsidieDataProvider } from "./DataProvider";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+root.render(
   <React.StrictMode>
-    <ThemeProvider>
+    <ThemeProvider
+      theme={{
+        ...ascDefaultTheme,
+        typography: { ...ascDefaultTheme.typography, fontFamily: "Amsterdam Sans, Arial, Helvetica, sans-serif" },
+      }}
+    >
       <GlobalStyle />
       <SubsidieDataProvider>
         <App />
       </SubsidieDataProvider>
     </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById("root"),
 );
